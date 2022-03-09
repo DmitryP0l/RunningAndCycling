dd5e19342ff1757b462e9b4bce1a44146dc895d5
DmitryP0l <dmitrypoletaeu@gmail.com>
2022-03-09 15:42:06 +0300

    commit

--- RunningAndCycling/Views/MapView.swift
+++ RunningAndCycling/Views/MapView.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit
 import CoreLocation
 
-final class MapView: UIView {
+final class MapView: UIView{
     
     private let containerView: UIView = {
         let view = UIView()
@@ -24,10 +24,12 @@ final class MapView: UIView {
     }()
     
     let coordinate = CLLocationCoordinate2D()
+    let manager = CLLocationManager()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
+        setupManager()
     }
     
     required init?(coder: NSCoder) {
@@ -37,13 +39,43 @@ final class MapView: UIView {
     private func setupView() {
         addSubview(containerView)
         containerView.addSubview(mapView)
-        
         containerView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-        
         mapView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+    }
+    
+    private func setupManager() {
+        manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    
+}
+
+
+extension MapView: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            render(location)
+        }
+    }
+    
+    func render(_ location: CLLocation) {
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
+                                                longitude: location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region,
+                          animated: true)
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        mapView.addAnnotation(pin)
     }
 }
